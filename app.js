d946cc35e592d8112cba713179b1f3960661936b
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2022-08-17 20:48:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+const cardNumber = document.querySelector(".heading");
+const namE = document.querySelector(".paragraph-1");
+const month = document.querySelector(".monthh");
+const  year = document.querySelector(".yearr");
+const cvc = document.querySelector(".cvc-details");
+
+
+const inputName = document.querySelector(".input-name");
+const inputNumber = document.querySelector(".input-card-number");
+const inputMonth = document.querySelector(".month");
+const inputYear = document.querySelector(".year");
+const inputCvc = document.querySelector(".input-cvc");
+const form = document.querySelector('.form');
+
+const btn = document.querySelector(".btn");
+
+
+const nameError = document.querySelector('.name-error');
+const cardError = document.querySelector('.card-error');
+const monthYear = document.querySelector('.exp-error');
+const cvcError = document.querySelector('.cvc-error');
+// const nameError = document.querySelector('.name-error');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+});
+
+inputName.addEventListener("input", (e) => {
+ namE.textContent = e.target.value 
+} );
+
+
+inputNumber.value.toString().replace(/\d{4}(?=.)/g, '$& ')
+inputNumber.addEventListener("input", (e) => {
+cardNumber.textContent = e.target.value.toString().replace(/\d{4}(?=.)/g, '$& ')
+ } );
+ 
+inputMonth.addEventListener("input", (e)=>{
+month.textContent = e.target.value 
+} );
+
+inputYear.addEventListener("input", (e)=>{
+    year.textContent = e.target.value 
+ } );
+    
+
+ inputCvc.addEventListener("input", (e)=>{
+    cvc.textContent = e.target.value 
+ } );
+
+
+function validation(){
+
+inputName.value === '' ?  nameError.classList.add("show-name-error") : nameError.classList.remove("show-name-error");  
+
+inputNumber.value === '' ? cardError.classList.add("show-card-error") : cardError.classList.remove("show-card-error");
+
+inputMonth.value === '' ?  monthYear.classList.add("show-exp-error") : monthYear.classList.remove("show-exp-error");
+
+inputYear.value === '' ?  monthYear.classList.add("show-exp-error") : monthYear.classList.remove("show-exp-error");
+
+inputCvc.value === '' ? cvcError.classList.add("show-cvc-error") : cvcError.classList.remove("show-cvc-error");
+
+
+    
+}
+
+
+ btn.addEventListener("click", () => {
+
+
+   validation()
+
+ })
