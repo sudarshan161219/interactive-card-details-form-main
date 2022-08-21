2ae33f5fd381dd52466f1cbfa9c6e2cef63b57ba
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2022-08-21 20:36:04 +0530

    update

--- app.js
+++ app.js
@@ -20,8 +20,11 @@ const continueBtn = document.querySelector(".continue")
 
 const nameError = document.querySelector('.name-error');
 const cardError = document.querySelector('.card-error');
+const cardError2 = document.querySelector('.card-error-2');
 const monthYear = document.querySelector('.exp-error');
+const monthYear2 = document.querySelector('.exp-error-2');
 const cvcError = document.querySelector('.cvc-error');
+const cvcError2 = document.querySelector('.cvc-error-2');
 
 
 form.addEventListener('submit', (e) => {
@@ -34,115 +37,135 @@ inputName.addEventListener("input", (e) => {
 });
 
 
-inputNumber.value.toString().replace(/\d{4}(?=.)/g, '$& ')
+inputNumber.toString().replace(/\d{4}(?=.)/g, '$& ')
 inputNumber.addEventListener("input", (e) => {
     cardNumber.textContent = e.target.value.toString().replace(/\d{4}(?=.)/g, '$& ')
+
+
+    var string = inputNumber.value;
+
+    for (let i = 0; i < string.length; i++) {
+        var keyCode = string[i];
+        var regex = /[^a-z]/g;
+        var isValid = regex.test(keyCode);
+        console.log(isValid);
+        if (!isValid) {
+            cardError2.classList.add("show-card-error-2")
+
+        } else {
+            cardError2.classList.remove("show-card-error-2")
+        }
+    }
+
 });
 
 inputMonth.addEventListener("input", (e) => {
     month.textContent = e.target.value
+    var string = inputMonth.value;
+
+    for (let i = 0; i < string.length; i++) {
+        var keyCode = string[i];
+        var regex = /[^a-z]/g;
+        var isValid = regex.test(keyCode);
+
+        if (!isValid) {
+            monthYear2.classList.add("show-exp-error-2")
+
+        } else {
+            monthYear2.classList.remove("show-exp-error-2")
+        }
+    }
 });
 
 inputYear.addEventListener("input", (e) => {
     year.textContent = e.target.value
+    var string = inputYear.value;
+
+    for (let i = 0; i < string.length; i++) {
+        var keyCode = string[i];
+        var regex = /[^a-z]/g;
+        var isValid = regex.test(keyCode);
+        if (!isValid) {
+            monthYear2.classList.add("show-exp-error-2")
+
+        } else {
+            monthYear2.classList.remove("show-exp-error-2")
+        }
+    }
 });
 
 
 inputCvc.addEventListener("input", (e) => {
     cvc.textContent = e.target.value
+    var string = inputCvc.value;
+
+    for (let i = 0; i < string.length; i++) {
+        var keyCode = string[i];
+        var regex = /[^a-z]/g;
+        var isValid = regex.test(keyCode);
+        console.log(isValid);
+        if (!isValid) {
+            cvcError2.classList.add("show-cvc-error-2")
+
+        } else {
+            cvcError2.classList.remove("show-cvc-error-2")
+        }
+    }
 });
+
+
 
 
 function validation() {
+    inputName.value === '' ? nameError.classList.add("show-name-error") || (inputName.style.border = '1px solid hsl(0, 100%, 66%)') : nameError.classList.remove("show-name-error") || (inputName.style.border = '1px solid #dfdee0');
 
-    if (inputName.value === '') {
-        inputName.style.border = '1px solid hsl(0, 100%, 66%)'
-    } else {
-        inputName.style.border = '1px solid #dfdee0'
-    }
+    inputNumber.value === '' ? cardError.classList.add("show-card-error") || (inputNumber.style.border = '1px solid hsl(0, 100%, 66%)') : cardError.classList.remove("show-card-error") || (inputNumber.style.border = '1px solid #dfdee0');
 
+    inputMonth.value === '' ? monthYear.classList.add("show-exp-error") || (inputMonth.style.border = '1px solid hsl(0, 100%, 66%)') : monthYear.classList.remove("show-exp-error") || (inputMonth.style.border = '1px solid #dfdee0');
 
-    if (inputNumber.value === '') {
-        inputNumber.style.border = '1px solid hsl(0, 100%, 66%)'
-    } else {
-        inputNumber.style.border = '1px solid #dfdee0'
-    }
+    inputYear.value === '' ? (monthYear.classList.add("show-exp-error")) || (inputYear.style.border = '1px solid hsl(0, 100%, 66%)') : monthYear.classList.remove("show-exp-error") || (inputYear.style.border = '1px solid #dfdee0');
 
-
-    if (inputMonth.value === '' || inputYear.value === '') {
-        inputMonth.style.border = '1px solid hsl(0, 100%, 66%)'
-        inputYear.style.border = '1px solid hsl(0, 100%, 66%)'
-    } else {
-        inputMonth.style.border = '1px solid #dfdee0'
-        inputYear.style.border = '1px solid #dfdee0'
-    }
-
-
-    if (inputCvc.value === '') {
-        inputCvc.style.border = '1px solid hsl(0, 100%, 66%)'
-    } else {
-        inputCvc.style.border = '1px solid #dfdee0'
-    }
-
-
-
-    if (input.value === namE.textContent) {
-
-        if (inputNumber.value === cardNumber.textContent) {
-
-            if (inputMonth.value === month.textContent) {
-
-                if (inputYear.value === year.textContent) {
-
-                    if (inputCvc.value === cvc.textContent) {
-                        form.classList.add("hide-form")
-                        thankyou.classList.add("show-thank-you")
-                    }
-
-                    form.classList.add("hide-form")
-                    thankyou.classList.add("show-thank-you")
-                }
-
-                form.classList.add("hide-form")
-                thankyou.classList.add("show-thank-you")
-            }
-
-            form.classList.add("hide-form")
-            thankyou.classList.add("show-thank-you")
-        }
-
-        form.classList.add("hide-form")
-        thankyou.classList.add("show-thank-you")
-
-    }
-
-    inputName.value === '' ? nameError.classList.add("show-name-error") : nameError.classList.remove("show-name-error");
-
-    inputNumber.value === '' ? cardError.classList.add("show-card-error") : cardError.classList.remove("show-card-error");
-
-    inputMonth.value === '' ? monthYear.classList.add("show-exp-error") : monthYear.classList.remove("show-exp-error");
-
-    inputYear.value === '' ? monthYear.classList.add("show-exp-error") : monthYear.classList.remove("show-exp-error");
-
-    inputCvc.value === '' ? cvcError.classList.add("show-cvc-error") : cvcError.classList.remove("show-cvc-error");
-
+    inputCvc.value === '' ? (cvcError.classList.add("show-cvc-error")) || (inputCvc.style.border = '1px solid hsl(0, 100%, 66%)') : (cvcError.classList.remove("show-cvc-error")) || (inputCvc.style.border = '1px solid #dfdee0');
 
 }
 
+function check() {
+
+    if (inputName.value.length > 3) {
+
+        form.classList.add('hide-form');
+        thankyou.classList.add('show-thank-you');
+
+        if (inputNumber.value.length <= 0) {
+            form.classList.remove('hide-form');
+            thankyou.classList.remove('show-thank-you');
+        }
+        if (inputMonth.value.length <= 0) {
+            form.classList.remove('hide-form');
+            thankyou.classList.remove('show-thank-you');
+        }
+
+        if (inputYear.value.length <= 0) {
+            form.classList.remove('hide-form');
+            thankyou.classList.remove('show-thank-you');
+        }
+        if (inputCvc.value.length <= 0) {
+            form.classList.remove('hide-form');
+            thankyou.classList.remove('show-thank-you');
+        }
+    }
+
+}
 
 btn.addEventListener("click", () => {
-
+    check()
     validation()
-})
-
-continueBtn.addEventListener('click', () => {
-   location.reload();
-inputName.value === ''
-inputNumber.value === ''
-inputMonth.value === ''
-inputYear.value === ''
-inputCvc.value === ''
-	form.classList.remove('hide-form');
-	thankyou.classList.remove('show-thank-you');
 });
 
+
+continueBtn.addEventListener('click', () => {
+    location.reload();
+    form.classList.remove('hide-form');
+    thankyou.classList.remove('show-thank-you');
+
+});
